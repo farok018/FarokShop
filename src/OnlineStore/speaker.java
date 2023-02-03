de2f05d847718549facaae08acd6b33dea9b0ab2
hairul <hairul@localhost>
2023-02-02 23:54:10 -0600

    the the pieces

---
+++ src/OnlineStore/speaker.java
@@ -0,0 +1,10 @@
+package OnlineStore;
+
+public class speaker {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("checking the number of pieces");
+	}
+
+}
