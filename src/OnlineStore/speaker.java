d48ecead62c6510c9e055794b7c631b6a4729d95
hairul <hairul@localhost>
2023-02-05 22:39:12 -0600

    check the work

--- src/OnlineStore/speaker.java
+++ src/OnlineStore/speaker.java
@@ -5,6 +5,9 @@ public class speaker {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("checking the number of pieces");
-	}
+System.out.println("make 100 pieces");
+System.out.println("clear");
 
-}
+
+
+
